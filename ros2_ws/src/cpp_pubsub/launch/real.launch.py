e3a70435dab992cba3b8c8c17bfc4e128509f93f
mpan31415 <michael.pan31415@gmail.com>
2023-08-09 15:39:46 +1000

    change to static broadcaster, update launch files

--- ros2_ws/src/cpp_pubsub/launch/real.launch.py
+++ ros2_ws/src/cpp_pubsub/launch/real.launch.py
@@ -7,6 +7,7 @@ from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
+
     robot_ip_parameter_name = 'robot_ip'
     load_gripper_parameter_name = 'load_gripper'
     use_fake_hardware_parameter_name = 'use_fake_hardware'
@@ -26,11 +27,11 @@ def generate_launch_description():
             description='Hostname or IP address of the robot.'),
         DeclareLaunchArgument(
             use_rviz_parameter_name,
-            default_value='false',                       ### this was originally false
+            default_value='true',                       ### this was originally false
             description='Visualize the robot in Rviz'),
         DeclareLaunchArgument(
             use_fake_hardware_parameter_name,
-            default_value='false',
+            default_value='true',
             description='Use fake hardware'),
         DeclareLaunchArgument(
             fake_sensor_commands_parameter_name,
@@ -43,6 +44,7 @@ def generate_launch_description():
             description='Use Franka Gripper as an end-effector, otherwise, the robot is loaded '
                         'without an end-effector.'),
 
+        ### franka_bringup launch ###
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource([PathJoinSubstitution(
                 [FindPackageShare('franka_bringup'), 'launch', 'franka.launch.py'])]),
@@ -52,6 +54,12 @@ def generate_launch_description():
                               fake_sensor_commands_parameter_name: fake_sensor_commands,
                               use_rviz_parameter_name: use_rviz
                               }.items(),
+        ),
+
+        ### kinect_camera launch ### 
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([PathJoinSubstitution(
+                [FindPackageShare('azure_kinect_ros_driver'), 'launch', 'driver.launch.py'])])
         ),
 
 
@@ -65,19 +73,26 @@ def generate_launch_description():
             output='screen',
         ),
 
-        # # start marker publisher
+        # publish camera frame
+        Node(
+            package='cpp_pubsub',
+            executable='const_br',
+            name='const_br'
+        ),
+
+        # activate Falcon node
+        # Node(
+        #     package='cpp_pubsub',
+        #     executable='position_talker',
+        #     name='position_talker'
+        # ),
+
+        # # marker publisher node
         # Node(
         #     package='cpp_pubsub',
         #     executable='marker_publisher',
         #     name='marker_publisher'
         # ),
-
-        # activate Falcon interface
-        Node(
-            package='cpp_pubsub',
-            executable='position_talker',
-            name='position_talker'
-        ),
 
         # # trajectory recorder node
         # Node(
--- ros2_ws/src/cpp_pubsub/src/const_br.cpp
+++ ros2_ws/src/cpp_pubsub/src/const_br.cpp
@@ -1,22 +1,13 @@
-#include <functional>
 #include <memory>
-#include <sstream>
-#include <string>
-#include <chrono>
 
 #include "geometry_msgs/msg/transform_stamped.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "tf2/LinearMath/Quaternion.h"
-#include "tf2_ros/transform_broadcaster.h"
+#include "tf2_ros/static_transform_broadcaster.h"
 
 
-using namespace std::chrono_literals;
-
-
-//////////////////////////////////////////////////////////////////////////////////////////////
-class FramePublisher : public rclcpp::Node
+class StaticFramePublisher : public rclcpp::Node
 {
-
 public:
 
   std::string panda_link_name = "panda_link0";
@@ -29,64 +20,54 @@ public:
   double rx = 0.0;
   double ry = 0.0;
   double rz = 3.14;
-  
 
-  FramePublisher()
-  : Node("camera_frame_broadcaster")
+  explicit StaticFramePublisher()
+  : Node("const_br")
   {
-    generate_transformation();
+    tf_static_broadcaster_ = std::make_shared<tf2_ros::StaticTransformBroadcaster>(this);
 
-    tf_broadcaster_ = std::make_unique<tf2_ros::TransformBroadcaster>(*this);
-
-    timer_ = this->create_wall_timer(100ms, std::bind(&FramePublisher::broadcast, this)); ///////// publishing at 10 Hz /////////
+    // Publish static transforms once at startup
+    this->make_transforms();
   }
 
 
 private:
 
-  void broadcast() {
-    // get current time-stamp & broadcast the transformation
-    trans_.header.stamp = this->get_clock()->now();
-    
-    std::cout << "Sending transformation now!" << std::endl;
-    tf_broadcaster_->sendTransform(trans_);
-  }
-
-
-  void generate_transformation()
-  {
-    // initialize message
-    trans_.header.stamp = this->get_clock()->now();
-    trans_.header.frame_id = panda_link_name;
-    trans_.child_frame_id = camera_link_name;
+  void make_transforms()
+  { 
+    // generate message
+    geometry_msgs::msg::TransformStamped t;
+    t.header.stamp = this->get_clock()->now();
+    t.header.frame_id = panda_link_name;
+    t.child_frame_id = camera_link_name;
 
     // translations
-    trans_.transform.translation.x = dx;
-    trans_.transform.translation.y = dy;
-    trans_.transform.translation.z = dz;
+    t.transform.translation.x = dx;
+    t.transform.translation.y = dy;
+    t.transform.translation.z = dz;
 
     // rotations
     tf2::Quaternion q;
     q.setRPY(rx, ry, rz);
-    trans_.transform.rotation.x = q.x();
-    trans_.transform.rotation.y = q.y();
-    trans_.transform.rotation.z = q.z();
-    trans_.transform.rotation.w = q.w();
+    t.transform.rotation.x = q.x();
+    t.transform.rotation.y = q.y();
+    t.transform.rotation.z = q.z();
+    t.transform.rotation.w = q.w();
+
+    tf_static_broadcaster_->sendTransform(t);
   }
 
-  geometry_msgs::msg::TransformStamped trans_;
-  std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
-  rclcpp::TimerBase::SharedPtr timer_;
+  std::shared_ptr<tf2_ros::StaticTransformBroadcaster> tf_static_broadcaster_;
 
 };
 
 
 
-//////////////////////////////////////////////////////////////////////////////////////////////
 int main(int argc, char * argv[])
-{
+{ 
+  // initialize node and spin it
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<FramePublisher>());
+  rclcpp::spin(std::make_shared<StaticFramePublisher>());
   rclcpp::shutdown();
   return 0;
 }
