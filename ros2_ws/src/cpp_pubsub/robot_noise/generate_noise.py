59cb311be5e4969017aaee9e5d93e2a7884810b7
mpan31415 <michael.pan31415@gmail.com>
2024-02-06 12:06:51 +1100

    noise generation update

--- ros2_ws/src/cpp_pubsub/robot_noise/generate_noise.py
+++ ros2_ws/src/cpp_pubsub/robot_noise/generate_noise.py
@@ -2,56 +2,68 @@ from scipy.ndimage import gaussian_filter1d
 from numpy.random import default_rng
 from numpy import asarray
 import matplotlib.pyplot as plt
-from interpolation import cosine_interp_list, linear_interp_list
+# from interpolation import cosine_interp_list, linear_interp_list
 # from os import getcwd
 
 ###### NOISE PARAMS ######
 NPOINTS = 101
 SIGMA = 4
 NUM_INTERP = 49
-SCALING = 30
+SCALING = 10
 
-FILE_INDEX = 4
+NUM_FILES = 10
 
-# file 1: sigma = 2, scaling = 50
-# file 2: sigma = 3, scaling = 50
-# file 3: sigma = 4, scaling = 30
+TARGET_ERR = 0.02444   # averaged from participants data [m]
+ERR_TOLERANCE = 0.003     # variance in participant error [m]
+START_TOLERANCE = 0.01   # first error point < 0.01 m
+
+
+def compute_my_rmse(noise_list):
+    abs_vals_list = [abs(num) for num in noise_list]
+    mean_err = sum(abs_vals_list) / len(abs_vals_list)
+    return mean_err
 
 
 def generate_noise(npoints: int, sigma: int):
     rng = default_rng()
     x = rng.normal(size=npoints)/SCALING
     y = gaussian_filter1d(x, sigma)
-    print("Successfully generated %d noise datapoints!" % len(y))
+    # print("Successfully generated %d noise datapoints!" % len(y))
     return x, y
 
 
 def main():
     
-    original, smoothed = generate_noise(NPOINTS, SIGMA)
+    valid_files = 0
+    file_index = 1
     
-    smoothed_arr = asarray(smoothed)
-    smoothed_arr.tofile('robot_noise/noise_csv_files/noise'+str(FILE_INDEX)+'.csv', sep=',')
-    print("Successfully saved noise datapoints to file = noise%d.csv!" % FILE_INDEX)
-    
-    old_indices = [i+1 for i in range(len(smoothed))]
-    new_indices = linear_interp_list(old_indices, NUM_INTERP)
-    # new_noise = cosine_interp_list(smoothed, NUM_INTERP)
-    new_noise = linear_interp_list(smoothed, NUM_INTERP)
+    while valid_files < NUM_FILES:
+        original, smoothed = generate_noise(NPOINTS, SIGMA)
+        smoothed_arr = asarray(smoothed)
+        noise_list = smoothed_arr.tolist()      # in [m]
+        my_rmse = compute_my_rmse(noise_list)   # in [m]
 
-    print("Total number of new datapoints = %d" % len(new_noise))
-    
-    plt.plot(old_indices, original, 'k', label='original')
-    plt.plot(old_indices, smoothed, 'bx-', label='smoothed')
-    
-    plt.plot(new_indices, new_noise, 'ro-', label='cosine interp')
-    
-    plt.legend()
-    plt.grid()
-    plt.show()
-
-    plt.savefig("robot_noise/plots/noise"+str(FILE_INDEX)+'.png')
-    print("Successfully saved noise plot to file = noise%d.png!" % FILE_INDEX)
+        if abs(my_rmse - TARGET_ERR) < ERR_TOLERANCE and abs(noise_list[0]) < START_TOLERANCE:
+            smoothed_arr.tofile('robot_noise/noise_csv_files/noise'+str(file_index)+'.csv', sep=',')
+            print("My RMSE = %.3f, Saved to file = noise%d.csv !" % (my_rmse, file_index))
+            valid_files += 1
+            file_index += 1
+        else:
+            print("Failed to generate noise to within the error tolerance!")
+        
+        # old_indices = [i+1 for i in range(len(smoothed))]
+        # new_indices = linear_interp_list(old_indices, NUM_INTERP)
+        # # new_noise = cosine_interp_list(smoothed, NUM_INTERP)
+        # new_noise = linear_interp_list(smoothed, NUM_INTERP)
+        
+        # plt.plot(old_indices, original, 'k', label='original')
+        # plt.plot(old_indices, smoothed, 'bx-', label='smoothed')
+        
+        # plt.plot(new_indices, new_noise, 'ro-', label='cosine interp')
+        
+        # plt.legend()
+        # plt.grid()
+        # plt.show()
     
 
 
